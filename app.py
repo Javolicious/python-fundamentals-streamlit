da26deba493bfc4d8accbb31decae8fea9e3a66a
Javolicious <38119056+Javolicious@users.noreply.github.com>
2026-08-23 13:01:54 -0500

    Remove duplicate author line in app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import streamlit as st
 st.title("Especialización Python for Analytics")
 st.sidebar.title("Ejercicios")
 st.write("Elaborado por Javier Artieda")
+st.write("Elaborado por Javier Artieda")
 st.image(
 "descargas/dmc.JPG",
 width=250
