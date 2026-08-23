cec1897b88ef42416ab664fbf99bb48dc99b500b
Javolicious <38119056+Javolicious@users.noreply.github.com>
2026-08-23 12:59:57 -0500

    Add module title and image to app
    Add module title and image to the app.

--- app.py
+++ app.py
@@ -4,3 +4,8 @@ import streamlit as st
 st.title("Especialización Python for Analytics")
 st.sidebar.title("Ejercicios")
 st.write("Elaborado por Javier Artieda")
+st.Write("Módulo 1")
+st.image(
+"descargas/dmc.JPG",
+width=250
+)
